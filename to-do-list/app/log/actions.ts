1863119191c13ca494a472227fe56e1dc1b8cc5c
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-06-20 12:12:08 +0200

    login system is done

---
+++ to-do-list/app/log/actions.ts
@@ -0,0 +1,82 @@
+'use server'
+
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+var sha256 = require('js-sha256');
+
+const cookieStore = cookies()
+
+const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+        cookies: {
+            get(name: string) {
+                return cookieStore.get(name)?.value
+            },
+        },
+    }
+)
+
+
+
+
+
+interface LogArg {
+    username: string,
+    password: string
+}
+
+export async function login(arg: LogArg) {
+    arg.password = sha256(arg.password)
+    console.log(arg)
+
+    let { data: Users, error } = await supabase
+        .from('Users')
+        .select("*")
+        .eq('username', arg.username)
+        .eq('password', arg.password)
+
+
+    if (Users.length == 1) {
+        console.log(true)
+        return true
+
+    }else{
+        let str: string = 'Your credentials does not match our system'
+        console.log(str)
+        return str
+    }
+
+
+}
+
+export async function register(arg: LogArg) {
+    arg.password = sha256(arg.password)
+    console.log(arg)
+
+
+    let { data: Users, error } = await supabase
+        .from('Users')
+        .select("*")
+        .eq('username', arg.username)
+
+
+    if (Users.length == 0) {
+
+        const { data, error } = await supabase
+            .from('Users')
+            .insert([
+                arg,
+            ])
+            .select()
+
+            return  true
+
+    }else{
+        console.log('already a user')
+        let str: string = 'Username already taken'
+        return str
+    }
+
+}
---
+++ to-do-list/app/log/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useState } from 'react'
+import { login, register } from './actions'
+import { useRouter } from 'next/navigation'
+
+export default function Log() {
+    const router = useRouter()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMsg, setErrorMsg] = useState('')
+
+
+
+    async function Login() {
+        if (username == '' || password == '') {
+            setErrorMsg('You need a password and an username')
+        }else{
+            let data = { username: username, password: password }
+            let result: string | boolean = await login(data)
+            if(result === true){
+                router.push('/')
+            }else{
+                setErrorMsg(result)
+            }
+        }
+    }
+
+
+    async function Register() {
+        if (username == '' || password == '') {
+            setErrorMsg('You need a password and an username')
+        }else{
+            let data = { username: username, password: password }
+            let result: string | boolean = await register(data)
+            if(result === true){
+                router.push('/')
+            }else{
+                setErrorMsg(result)
+            }
+        }
+    }
+
+    return (
+        <form onSubmit={e => { e.preventDefault() }}>
+            <p>{errorMsg}</p>
+            <input type="text" value={username} onChange={e => { setUsername(e.target.value) }}  placeholder='Username'/>
+            <input type="password" value={password} onChange={e => { setPassword(e.target.value) }}  placeholder='Password'/>
+            <button onClick={Login}>Login</button>
+            <button onClick={Register}>Register</button>
+        </form>
+    )
+}
